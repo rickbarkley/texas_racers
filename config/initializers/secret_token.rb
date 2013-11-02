b3125c14f454b1bd713ba7f1a55ddb86dd5f81ac
rick_barkley <rick_barkley@nuvola-networks.com>
2013-11-02 17:10:43 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TexasRacers::Application.config.secret_token = '697fb16db171c12ae66c86fd58ed8944a6b9f72c1707b2b5eeeed3bb839a1c7e6681ec9ed44479d216d26bdf2dd6b152063104a91b58d402a090ffe414d74573'
